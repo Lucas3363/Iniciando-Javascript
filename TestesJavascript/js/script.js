413c6ad00ed13d0c6da9b64c5e3b4f5149ebf849
Lucas3363 <90220756+Lucas3363@users.noreply.github.com>
2022-03-14 19:50:52 -0300

    primeiros testes

---
+++ TestesJavascript/js/script.js
@@ -0,0 +1,13 @@
+function teste(){
+    document.getElementById('info').innerHTML = 'Texto novvo porra';
+}
+
+alert('Salveeeee')
+
+function teste(){
+    document.getElementById('titulo').innerHTML='Texto original rs';
+}
+
+function testando2(){
+    document.getElementById("mudaremail").innerHTML= <p>novo paragrafo garaiiiiiiiiii</p>
+}
