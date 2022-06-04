5cdad7f361501366c57aec4d5643464fa400a829
Lucas3363 <90220756+Lucas3363@users.noreply.github.com>
2022-06-04 17:41:18 -0300

    API que carrega fotos no HTML

---
+++ TestesJavascript/DIO_API/assets/js/scripts.js
@@ -0,0 +1,42 @@
+const BASE_URL = 'https://thatcopy.github.io/catAPI/imgs/webp/2b74f7c.webp';
+const catBtn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const img = document.getElementsByTagName('img')[0];
+	img.src = await getCats();
+};
+
+loadImg();
+
+const btn = document.getElementById('change-cat');
+btn.addEventListener('click', loadImg);
+
+// const BASE_URL = 'https://thatcopy.github.io/catAPI/imgs/webp/2b74f7c.webp';
+// const catBtn = document.getElementById('change-cat');
+
+// const getCats = async () => {
+// 	const data = await fetch(BASE_URL);
+// 	    then((res) => res.json())
+//         .catch((e) => console.log(e));
+
+//     return data.webpurl;
+// };    
+
+// const loadImg = async () => {
+// 	const catImg = document.getElementsByTagName('cat');
+// 	catImg.src = await getCats();
+// };
+
+// catBtn.addEventListener('click', loadImg)
+
+// loadImg();
